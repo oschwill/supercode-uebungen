0597b779bdfa18631a5dae69606984cba57bf576
Oliver Schwill <o.schwill@web.de>
2023-09-13 13:43:46 +0200

    forms with js

--- javascript-uebungen/forms/Level-3_3/assets/js/main.js
+++ javascript-uebungen/forms/Level-3_3/assets/js/main.js
@@ -1,16 +1,12 @@
 /* CONTAINER */
 const mainContainer = document.querySelector('main');
 /* DATA */
-const colorPicker = {
-  red: [0, 33, 66, 100],
-  green: [0, 33, 66, 100],
-  blue: [0, 33, 66, 100],
-};
+const colorPicker = [0, 33, 66, 100];
 
 function changeBackground() {
   const redRange = document.querySelector('#red').value;
   const greenRange = document.querySelector('#green').value;
   const blueRange = document.querySelector('#blue').value;
 
-  mainContainer.style.backgroundColor = `rgb(${colorPicker.red[redRange]}%,${colorPicker.red[greenRange]}%,${colorPicker.red[blueRange]}%)`;
+  mainContainer.style.backgroundColor = `rgb(${colorPicker[redRange]}%,${colorPicker[greenRange]}%,${colorPicker[blueRange]}%)`;
 }
