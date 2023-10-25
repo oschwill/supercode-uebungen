0b3f5e62dc460a84ed96662ae19b6cab1b1e2a84
Oliver Schwill <o.schwill@web.de>
2023-10-25 14:57:15 +0200

    beer api

--- projects/beer-api/src/components/beerList/BeerListItems.jsx
+++ projects/beer-api/src/components/beerList/BeerListItems.jsx
@@ -3,22 +3,6 @@ import { Link } from 'react-router-dom';
 
 /* CSS */
 import './BeerListItems.css';
-
-// const BeerListItems = ({ beer }) => {
-//   return (
-//     <div className="beer-items">
-//       <div className="left-side">
-//         <img src={beer.image_url} alt={beer.name} />
-//       </div>
-//       <div className="right-side">
-//         <h2>{beer.name}</h2>
-//         <p>{beer.tagline}</p>
-//         <p>created by: {beer.name}</p>
-//         <Link to={`/beer-detail/${beer._id}`}>Details</Link>
-//       </div>
-//     </div>
-//   );
-// };
 const BeerListItems = ({ beerData }) => {
   return (
     <>
@@ -45,8 +29,5 @@ const BeerListItems = ({ beerData }) => {
 BeerListItems.propTypes = {
   beerData: PropTypes.array,
 };
-// BeerListItems.propTypes = {
-//   beer: PropTypes.object,
-// };
 
 export default BeerListItems;
