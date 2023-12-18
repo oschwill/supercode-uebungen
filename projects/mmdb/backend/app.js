c172086d5a0eba59bb8fd4aa6f04c17adfce478a
oschwill <o.schwill@web.de>
2023-12-18 14:21:55 +0100

    limit erhöht

--- projects/mmdb/backend/app.js
+++ projects/mmdb/backend/app.js
@@ -18,7 +18,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '50kb' })); // limitieren die Requestgröße
+app.use(express.json({ limit: '5000kb' })); // limitieren die Requestgröße
 app.use(xss()); // Schutz vor xss Attacken
 // maximal 50 request pro Stunde von einer IP Adresse erlauben
 const limiter = rateLimit({
